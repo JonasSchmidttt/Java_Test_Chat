e5fb2baadf5d89eecccb9a4cceede9559dc8e70e
Jonas Schmidt <jonas.schmidt@hebk.de>
2023-05-22 16:45:38 +0200

    init

---
+++ client/src/main/java/org/client/SimpleClient.java
@@ -0,0 +1,17 @@
+package org.client;
+
+import java.net.*;
+import java.io.*;
+
+public class SimpleClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("google.com", 80);
+            byte[] data = test.
+            output.write(data);
+        }catch (IOException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
+}
